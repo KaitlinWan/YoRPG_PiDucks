85076e27e41dcde4e8ffb33d5322cd5327e410cd
Kaitlin <kwan1@stuy.edu>
2017-11-14 06:24:11 -0500

    Updated Monsters

--- v3/Ghost.java
+++ v3/Ghost.java
@@ -1,4 +1,17 @@
 public class Ghost extends Monster{
 //ATTACK
 
+	public Ghost(){
+		_hitPts = 150;
+		_strength = 20;
+		_defense = 10;
+		_attack = 1.5;
+
+	}
+
+	public static String about(){
+		String retStr = "The Ghost class has a greater attack. His attack is by far the best of all classes. He lacks in strength because sometimes his hand just floats through his prey."
+		return retStr;
+	}
+
 }
